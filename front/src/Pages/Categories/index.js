0dbb459309624a5308527c66c6ab57e01c1c6dcc
이준희 <junh0328@naver.com>
2021-05-13 15:23:21 +0900

    #3 settings: emotion, antd 적용

--- front/src/Pages/Categories/index.js
+++ front/src/Pages/Categories/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Button } from './style';
 
 const Categories = () => {
   return (
     <div>
       <h1>Categories 페이지입니다.</h1>
+      <Button>버튼 1</Button>
     </div>
   );
 };
