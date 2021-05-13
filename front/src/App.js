8ad093aa3e699f185307046f6427379825f4691a
이준희 <junh0328@naver.com>
2021-05-13 15:14:39 +0900

    #3 settings: eslint + prettier 적용

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Main from "./Pages/Main";
-import Categories from "./Pages/Categories";
-import Submit from "./Pages/Submit";
-import Private from "./Pages/Private";
+import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Main from './Pages/Main';
+import Categories from './Pages/Categories';
+import Submit from './Pages/Submit';
+import Private from './Pages/Private';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact={true} component={Main} />
+        <Route path="/" exact component={Main} />
         <Route path="/categories" component={Categories} />
         <Route path="/submit" component={Submit} />
         <Route path="/private" component={Private} />
