312e51bfb09c702a74ed2c3519d9cc21e29ee9d1
Dohae <dode777.dev@gmail.com>
2021-06-18 00:51:12 +0900

    Feat: 프록시 세팅

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const cors = require('cors');
+const morgan = require('morgan');
 
 require("./config/mongoose.js");
 
@@ -13,6 +15,30 @@ const swaggerSpec = swaggerJsdoc(swagger)
 const userRouter = require('./routers/userRouter');
 
 app.use(express.json())
+// Proxy set
+app.set('trust proxy', 1);
+if (process.env.NODE_ENV === 'production') {
+  // Product
+  app.use(morgan('combined'));
+  app.use(helmet());
+  app.use(
+    cors({
+      origin: ['http://example.com'],
+      credentials: true,
+    })
+  );
+} else {
+  // Develop
+  app.use(morgan('dev'));
+  app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  );
+}
+
+app.use(express.json());
 
 app.use('/api/v1/', userRouter);
 
