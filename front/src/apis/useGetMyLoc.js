1a8b53e46a5ff81f403c08c6fcae80d0b9f4aa41
이준희 <junh0328@naver.com>
2021-06-17 19:23:14 +0900

    [#16] feat: 마커당 클릭 이벤트 기본 설정 및 더미데이터 id 추가

--- front/src/apis/useGetMyLoc.js
+++ front/src/apis/useGetMyLoc.js
@@ -31,6 +31,7 @@ export function useGetMyLoc() {
   }
 
   function displayMarker(positions) {
+    console.log('positions 배열 출력:', positions);
     for (var i = 0; i < positions.length; i++) {
       const imageSize = new kakao.maps.Size(40, 60);
 
@@ -55,27 +56,34 @@ export function useGetMyLoc() {
       // 지도 중심좌표를 접속위치로 변경합니다
       map.setCenter(positions[0].latlng, marker);
 
-      // console.log('함수 내에서 map 객체 출력: ', map);
+      kakao.maps.event.addListener(marker, 'click', () => {
+        console.log('maker:', marker);
+        console.log('marker.Fb:', marker.Fb);
+      });
     }
   }
 
   // 더미데이터이기 때문에 useGetMyLoc 함수 내에 적용하였습니다
   const positions = [
     {
+      id: 1,
       title: '내 위치',
       latlng: null,
     },
     {
+      id: 2,
       title: '내손 의왕 메가커피',
       latlng: new kakao.maps.LatLng(37.38992745536002, 126.97743015243483),
       img: coca,
     },
     {
+      id: 3,
       title: '평촌동 두산벤쳐다임',
       latlng: new kakao.maps.LatLng(37.39124205567942, 126.97296865595483),
       img: pepsi,
     },
     {
+      id: 4,
       title: '내손 의왕 스타벅스',
       latlng: new kakao.maps.LatLng(37.38903279939199, 126.97623476944985),
       img: coca,
