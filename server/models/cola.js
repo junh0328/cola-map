3ebd9a57874097a724689e2203000eb713146e5a
Dohae <dode777.dev@gmail.com>
2021-06-18 00:59:44 +0900

    Feat: 콜라 스키마 추가

---
+++ server/models/cola.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const colaSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Cola = mongoose.model('Cola', colaSchema);
+
+module.exports = Cola;
