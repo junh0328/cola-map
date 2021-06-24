d29bfd37ce27640bd0a58462e75106d425bd45b3
이준희 <junh0328@naver.com>
2021-06-24 21:04:38 +0900

    [#31] feat: UserInfo 작업 완료 (이모션 및 useMemo을 통한 스타일링 및 클릭 이벤트 적용)

--- front/src/components/UserInfoBar/index.js
+++ front/src/components/UserInfoBar/index.js
@@ -1,11 +1,16 @@
-import { UserInfoWrapper } from './style';
+import { UserInfoWrapper, UserInfoWrapperMain } from './style';
 import { LogoutOutlined } from '@ant-design/icons';
+import { useMemo } from 'react';
 
 const UserInfoBar = () => {
+  const style = useMemo(() => ({ cursor: 'pointer', position: 'absolute', right: '3%' }), []);
+
   return (
     <UserInfoWrapper>
-      <b title="UserName">UserName</b>
-      <LogoutOutlined style={{ cursor: 'pointer' }} title="로그아웃" />
+      <UserInfoWrapperMain>
+        <span title="UserName">홍길동</span>
+        <LogoutOutlined style={style} title="로그아웃" onClick={() => alert('로그아웃 되었습니다!')} />
+      </UserInfoWrapperMain>
     </UserInfoWrapper>
   );
 };
--- front/src/components/UserInfoBar/style.js
+++ front/src/components/UserInfoBar/style.js
@@ -1,11 +1,22 @@
 import styled from '@emotion/styled';
 
 export const UserInfoWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: space-between;
   padding: 1em;
   border-bottom: 1px solid #ddd;
   font-size: 1.2rem;
+  color: #0f4c82;
+`;
+
+export const UserInfoWrapperMain = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  border: 3px solid #0f4c82;
+  padding: 10px;
+  padding-left: 3%;
+  border-radius: 30px;
+
+  & span {
+    font-weight: bolder;
+  }
 `;
