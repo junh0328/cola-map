073fae289b715b8c62376bd3007d208e02c148a0
Dohae <dode777.dev@gmail.com>
2021-07-13 10:33:53 +0900

    Feat: User Router, 유저 조회 및 삭제 라우터 추가

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,26 +1,54 @@
 const express = require('express');
 const User = require('../models/user');
 const userRouter = express.Router();
-const auth = require('../middleware/auth')
+const auth = require('../middleware/auth');
+
+userRouter.get('/', async (req, res) => {
+  try {
+    let user = await User.find({});
+    res.send({
+      AllOfUser: user,
+    });
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
+
+userRouter.delete('/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deletedUser = await User.findOneAndDelete({ _id: id });
+    res.send({
+      'Deleted User': deletedUser,
+    });
+  } catch (error) {
+    res.status(500).send({ error: error });
+  }
+});
 
 userRouter.post('/login', async (req, res) => {
   try {
-    const uniqId = req.query.uniqId
+    const uniqId = req.query.uniqId;
 
-    let user = await User.findOne({ uniqId: uniqId })
+    let user = await User.findOne({ uniqId: uniqId });
     if (!user) {
+      const avatarUrl = req.query.avatarUrl,
+        nickname = req.query.nickname,
+        kakaoMail = req.query.kakaoMail;
       const userObject = new User({
         uniqId: uniqId,
-        nickname: req.query.nickname
-      })
-      user = await userObject.save()
+        nickname: nickname,
+        avatarUrl: avatarUrl,
+        kakaoMail: kakaoMail,
+      });
+      user = await userObject.save();
     }
-    const token = await user.generateAuthToken()
-    res.send({ token: token })
+    const token = await user.generateAuthToken();
+    res.send({ token: token });
   } catch (e) {
     res.status(500).send({ message: e.message });
   }
-})
+});
 
 // 로그아웃 기능은 프론트단에서 버튼 클릭 시 header에 authorizaiton 값을 지워주시면 될거같아요.
 
@@ -34,6 +62,5 @@ userRouter.post('/login', async (req, res) => {
 //     res.status(500).send({message: e.message});
 //   }
 // })
-
 
 module.exports = userRouter;
