a32c548768a3a60f563ad5695d2ef182a1c9b896
Dohae <dode777.dev@gmail.com>
2021-07-16 16:46:00 +0900

    Fix: mongoose 오류 해결
    mongoose > FindOneAndUpdate 명령어 사용 도중 문제발생
    userFindAndModify: false 구문 추가

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
     console.log('✅ DB is connected');
   } catch (error) {
     console.log(error);
