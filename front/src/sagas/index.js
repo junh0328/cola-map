a2e383b81bdc26b605fdd37d163838b58b0c75a8
이준희 <junh0328@naver.com>
2021-07-23 17:23:03 +0900

    [#54] fix: axios 요청 시 헤더에 토큰 추가(글로벌로 참조 가능)

--- front/src/sagas/index.js
+++ front/src/sagas/index.js
@@ -8,6 +8,13 @@ import postSaga from './post';
 axios.defaults.baseURL = 'http://localhost:5000';
 axios.defaults.withCredentials = true;
 
+/* 헤더에 넣어 보낼 사용자의 토큰 */
+export let myConfig = {
+  headers: {
+    Authorization: 'Bearer ' + localStorage.getItem('token'),
+  },
+};
+
 export default function* rootSaga() {
   yield all([fork(mapSaga), fork(personalSaga), fork(postSaga)]);
 }
