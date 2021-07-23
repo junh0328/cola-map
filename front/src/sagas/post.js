f0edaa77e0f78350ebe8c0a390e896417f0f96ef
이준희 <junh0328@naver.com>
2021-07-23 17:23:03 +0900

    [#54] fix: axios 요청 시 헤더에 토큰 추가(글로벌로 참조 가능)

--- front/src/sagas/post.js
+++ front/src/sagas/post.js
@@ -2,10 +2,12 @@ import axios from 'axios';
 import { POST_STORE_FAILURE, POST_STORE_REQUEST, POST_STORE_SUCCESS } from 'reducers/post';
 
 import { all, call, fork, put, takeLatest } from 'redux-saga/effects';
+import { myConfig } from 'sagas';
 
 function postStoreRequestAPI(data) {
+  console.log('check before axios request:', data, localStorage.getItem('token'));
   try {
-    return axios.post('/post', data);
+    return axios.post('/post', data, myConfig);
   } catch (err) {
     console.log(err);
   }
