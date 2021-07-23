5e674171f5414310dbd56a0e02706ef7c2c483f5
Dohae <dode777.dev@gmail.com>
2021-07-23 10:04:50 +0900

    Feat: 기존 로그인&회원가입 기능 삭제
    로그 아웃 관련 주석도 삭제했습니다.
    
    로그 아웃은 프론트단에서 로컬스토리지 값을 지워주는 것으로 동작합니다.

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const userRouter = express.Router();
 
-const User = require('../models/user');
 const auth = require('../middleware/auth');
 
 const axios = require('axios');
@@ -75,6 +74,7 @@ userRouter.get('/kakao/callback', async (req, res) => {
     res.status(500).send(e.message);
   }
 });
+
 // 임시 API - 모든 유저 조회
 userRouter.get('/', async (req, res) => {
   try {
@@ -134,42 +134,5 @@ userRouter.delete('/quit', async (req, res) => {
     res.status(500).send({ error: e.message });
   }
 });
-
-// 로그인 & 회원가입
-userRouter.post('/login', async (req, res) => {
-  const uniqId = req.body.uniqId;
-  try {
-    let user = await User.findOne({ uniqId: uniqId });
-    if (!user) {
-      const profile_image = req.body.profile_image,
-        profile_nickname = req.body.profile_nickname,
-        account_email = req.body.account_email;
-      const userObject = new User({
-        uniqId: uniqId,
-        profile_image: profile_image,
-        profile_nickname: profile_nickname,
-        account_email: account_email,
-      });
-      user = await userObject.save();
-    }
-    const token = await user.generateAuthToken();
-    res.send({ token: token });
-  } catch (e) {
-    res.status(500).send({ message: e.message });
-  }
-});
-
-// 로그아웃 기능은 프론트단에서 버튼 클릭 시 header에 authorizaiton 값을 지워주시면 될거같아요.
-
-// router.post('/user/logout', auth, async (req, res) => {
-//   try {
-//     User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
-//       if (err) return res.json({ success: false, err });
-//       return res.status(200).send({ success: true });
-//     });
-//   } catch (e) {
-//     res.status(500).send({message: e.message});
-//   }
-// })
 
 module.exports = userRouter;
