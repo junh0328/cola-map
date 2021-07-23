50629f20532d21fa1d4ab1477629846b1b259126
Dohae <dode777.dev@gmail.com>
2021-07-23 09:59:55 +0900

    Feat: 카카오 인증 로직 구현
    인가 코드 발행 / 카카오 토큰 발행 / 유저 정보 조회

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -4,6 +4,52 @@ const userRouter = express.Router();
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 
+const axios = require('axios');
+const qs = require('qs');
+
+// 카카오 로그인 로직
+userRouter.get('/kakao/login', async (req, res) => {
+  const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_KEY}&redirect_uri=${process.env.KAKAO_REDIRECT_URL}&response_type=code&scope=profile,account_email`;
+
+  // const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_KEY}&redirect_uri=${process.env.KAKAO_REDIRECT_URL}&response_type=code&scope=profile_nickname,profile_image,account_email`;
+  // 비즈니스 앱
+  res.redirect(kakaoAuthURL);
+});
+
+userRouter.get('/kakao/callback', async (req, res) => {
+  // kakao 토큰 발급
+  let kakaoToken;
+  try {
+    kakaoToken = await axios({
+      method: 'POST',
+      url: 'https://kauth.kakao.com/oauth/token',
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      data: qs.stringify({
+        grant_type: 'authorization_code',
+        client_id: process.env.KAKAO_KEY,
+        redirectUri: process.env.KAKAO_REDIRECT_URL,
+        code: req.query.code,
+      }),
+    });
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+
+  // kakao 토큰으로 유저 정보 확인
+  let user;
+  try {
+    user = await axios({
+      method: 'get',
+      url: 'https://kapi.kakao.com/v2/user/me',
+      headers: {
+        Authorization: `Bearer ${kakaoToken.data.access_token}`,
+      },
+    });
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
 // 임시 API - 모든 유저 조회
 userRouter.get('/', async (req, res) => {
   try {
