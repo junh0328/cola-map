24f3b26062d0f8801a83397f63c548bd3b35464f
이준희 <junh0328@naver.com>
2021-07-24 17:45:22 +0900

    [#54] feat: GET 방식, 가게 정보 데이터 호출 및 렌더링 로직 구현

--- front/src/reducers/post.js
+++ front/src/reducers/post.js
@@ -1,12 +1,21 @@
 import produce from 'immer';
 
 export const initialState = {
-  data: [],
+  data: null,
+  storeData: [],
+
+  getStoreLoading: false,
+  getStoreSuccess: false,
+  getStoreError: null,
 
   postStoreLoading: false,
   postStoreSuccess: false,
   postStoreError: null,
 };
+
+export const GET_STORE_REQUEST = 'GET_STORE_REQUEST';
+export const GET_STORE_SUCCESS = 'GET_STORE_SUCCESS';
+export const GET_STORE_FAILURE = 'GET_STORE_FAILURE';
 
 export const POST_STORE_REQUEST = 'POST_STORE_REQUEST';
 export const POST_STORE_SUCCESS = 'POST_STORE_SUCCESS';
@@ -15,6 +24,22 @@ export const POST_STORE_FAILURE = 'POST_STORE_FAILURE';
 const post = (state = initialState, action) =>
   produce(state, (draft) => {
     switch (action.type) {
+      case GET_STORE_REQUEST: {
+        draft.getStoreLoading = true;
+        draft.getStoreSuccess = false;
+        break;
+      }
+      case GET_STORE_SUCCESS: {
+        draft.getStoreLoading = false;
+        draft.getStoreSuccess = true;
+        draft.storeData = draft.storeData.concat(action.data);
+        break;
+      }
+      case GET_STORE_FAILURE: {
+        draft.getStoreLoading = false;
+        draft.getStoreError = action.error;
+        break;
+      }
       case POST_STORE_REQUEST: {
         draft.postStoreLoading = true;
         draft.postStoreSuccess = false;
@@ -23,7 +48,7 @@ const post = (state = initialState, action) =>
       case POST_STORE_SUCCESS: {
         draft.postStoreLoading = false;
         draft.postStoreSuccess = true;
-        draft.data = draft.data.concat(action.data);
+        draft.data = action.data;
         break;
       }
       case POST_STORE_FAILURE: {
