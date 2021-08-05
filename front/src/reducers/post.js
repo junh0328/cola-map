e88c93e36067610b30319ccff539296db7dec308
이준희 <junh0328@naver.com>
2021-08-05 21:18:20 +0900

    [#78] feat: 게시글 삭제하기 기능 추가

--- front/src/reducers/post.js
+++ front/src/reducers/post.js
@@ -11,6 +11,10 @@ export const initialState = {
   postStoreLoading: false,
   postStoreSuccess: false,
   postStoreError: null,
+
+  deletePostLoading: false,
+  deletePostSuccess: false,
+  deletePostError: null,
 };
 
 export const GET_STORE_REQUEST = 'GET_STORE_REQUEST';
@@ -20,6 +24,10 @@ export const GET_STORE_FAILURE = 'GET_STORE_FAILURE';
 export const POST_STORE_REQUEST = 'POST_STORE_REQUEST';
 export const POST_STORE_SUCCESS = 'POST_STORE_SUCCESS';
 export const POST_STORE_FAILURE = 'POST_STORE_FAILURE';
+
+export const DELETE_POST_REQUEST = 'DELETE_POST_REQUEST';
+export const DELETE_POST_SUCCESS = 'DELETE_POST_SUCCESS';
+export const DELETE_POST_FAILURE = 'DELETE_POST_FAILURE';
 
 const post = (state = initialState, action) =>
   produce(state, (draft) => {
@@ -54,6 +62,23 @@ const post = (state = initialState, action) =>
       case POST_STORE_FAILURE: {
         draft.postStoreLoading = false;
         draft.postStoreError = action.error;
+        break;
+      }
+      case DELETE_POST_REQUEST: {
+        draft.deletePostLoading = true;
+        draft.deletePostSuccess = false;
+        break;
+      }
+      case DELETE_POST_SUCCESS: {
+        draft.deletePostLoading = false;
+        draft.deletePostSuccess = true;
+        draft.storeData = draft.storeData.filter((v) => v._id !== action.data);
+        // action.data 에는 610bce26764ea20f617a3b24 가 들어있음
+        break;
+      }
+      case DELETE_POST_FAILURE: {
+        draft.deletePostLoading = false;
+        draft.deletePostError = action.error;
         break;
       }
       default:
